4a95db235632d98876f2d1ef82fbec653ebc3eab
jakeia <2022302111153@whu.edu.cn>
2024-03-12 16:14:00 +0800

    remove others colorscheme

--- init.lua
+++ init.lua
@@ -19,9 +19,8 @@ return {
   },
 
   -- Set colorscheme to use
-  -- colorscheme = "tokyonight-night",
-  colorscheme = "catppuccin",
   -- colorscheme = "monokai-pro",
+  colorscheme = "sonokai",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
