cecc62b50be1957ed49828aeca279d4865b2d3ec
jakeia <2022302111153@whu.edu.cn>
2024-03-13 18:30:56 +0800

    change colorscheme

--- plugins/sonokai.lua
+++ plugins/sonokai.lua
@@ -1,4 +1,7 @@
 return {
   "sainnhe/sonokai",
-  init = function() vim.g.sonokai_dim_inactive_windows = 1 end,
+  init = function()
+    vim.g.sonokai_dim_inactive_windows = 1
+    vim.g.sonokai_style = "maia"
+  end,
 }
