02d981464a9cf65be125ba88731b30a7fe841c2d
jakeia <2022302111153@whu.edu.cn>
2024-03-19 20:25:01 +0800

    change notify config

--- plugins/notify.lua
+++ plugins/notify.lua
@@ -2,7 +2,12 @@ return {
   "rcarriga/nvim-notify",
   opts = {
 
-    background_colour = "#000000",
+    -- background_colour = "#000000",
     timeout = 1000,
+    stages = "static",
+    render = "compact",
+    max_width = "30",
+    fps = 5,
+    level = 1,
   },
 }
